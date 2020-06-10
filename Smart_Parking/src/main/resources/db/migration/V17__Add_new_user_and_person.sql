ab12a2be74f3cb4ba1adcd221915f05c35a1cfd2
Maksim Davletkulov <m.davlekulov@nominex.io>
2020-06-10 22:46:37 +0400

    Add new data in db

---
+++ Smart_Parking/src/main/resources/db/migration/V17__Add_new_user_and_person.sql
@@ -0,0 +1,14 @@
+insert into usr (username, password, enabled, first_name, second_name, middle_name, role)
+values ('tolmachevvn@sstu.ru', '12345678Ab', 1, N'Владимир', N'Толмачев', N'Николаевич', 'ADMIN');
+
+update usr set password = dbo.Bcrypt(password,8) where second_name = N'Толмачев';
+
+insert into job_position(name_position)
+values (N'Директор института');
+
+insert into person(course, employee, first_name, second_name, middle_name, group_name, pass_end_date,
+                   pass_num, special_status, student, job_position, subdivision, division)
+values (null, 1, N'Ольга', N'Долинина', N'Николаевна', null, DATEADD(month, 6, current_timestamp), 25, 0, 0, (select id from job_position where name_position = N'Директор института'), 16, 4);
+insert into automobile(model, number, color_id, person_id)
+
+values ('Nissan X-Trail', 'H380HP64', 4, (select id from person where second_name = N'Долинина'));
